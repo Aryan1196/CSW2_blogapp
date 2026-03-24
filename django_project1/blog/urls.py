aa2e1062b4da345d308880eed97aa62063bc2cb9
ARYAN KUMAR SAHU <aryanaks0007@gmail.com>
2026-03-24 12:54:14 +0530

    added the frontend post method

--- django_project1/blog/urls.py
+++ django_project1/blog/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
 # path('', views.home, name='blog-home'),
 path('about/', views.about, name='about-page'),
 path('', views.PostListView.as_view(), name='blog-home'),
+path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+path('post/new/', views.PostCreateView.as_view(), name='post-create'),
 ]
--- django_project1/blog/views.py
+++ django_project1/blog/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from blog.models import Post
-from django.views.generic import ListView
+from django.views.generic import (ListView,DetailView,CreateView)
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+   model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+   model = Post
+   fields = ['title' , 'content']
+   def form_valid(self,form):
+      form.instance.author = self.request.user
+      return super().form_valid(form)
+
+
+
 
 def home(request):
    context = {
